61a09ba5d140c16344890061714e98a6ae6554ef
lxlan <lxlandsx@outlook.com>
2022-04-12 11:38:56 +0800

    SpringBoot 配置文件设置, 以及配置的读取

--- SpringBootConfig/src/main/java/com/github/controller/BookController.java
+++ SpringBootConfig/src/main/java/com/github/controller/BookController.java
@@ -1,7 +1,11 @@
 package com.github.controller;
 
+import com.github.MyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +22,32 @@ public class BookController {
     @Value("${user.age}")
     private int age;
 
+    @Value("${tempDir}")
+    private String tempDir;
+
+    // 使用自动装配将所有的数据封装到一个Environment对象当中去
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private MyDataSource myDataSource;
+
     @GetMapping
     public String getById() {
         System.out.println("你好，你是我第一个自己写出来的程序");
         System.out.println("country ==>" + country);
         System.out.println("name ==>" + username);
         System.out.println("age==>" + age);
+        System.out.println("tempDir==>" + tempDir);
+        System.out.println("=============================");
+        System.out.println(environment.getProperty("user.name"));
+        System.out.println(myDataSource);
         return "你好，xxx";
+    }
+
+    @PostMapping("testPost")
+    public void test(){
+        System.out.println("hah");
     }
 
 }
