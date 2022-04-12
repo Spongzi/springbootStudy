a2f34921adc5dbbf8026f935888b3120898c0d42
lxlan <lxlandsx@outlook.com>
2022-04-12 11:10:01 +0800

    SpringBoot 配置文件设置, 以及配置的读取

--- SpringBootConfig/src/main/java/com/github/controller/BookController.java
+++ SpringBootConfig/src/main/java/com/github/controller/BookController.java
@@ -1,2 +1,30 @@
-package com.github.controller;public class BookController {
+package com.github.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Value("${country}")
+    private String country;
+
+    @Value("${user.name}")
+    private String username;
+
+    @Value("${user.age}")
+    private int age;
+
+    @GetMapping
+    public String getById() {
+        System.out.println("你好，你是我第一个自己写出来的程序");
+        System.out.println("country ==>" + country);
+        System.out.println("name ==>" + username);
+        System.out.println("age==>" + age);
+        return "你好，xxx";
+    }
+
 }
