46ceed30eb90b88fb51fcac0bdcfa9185dd19a3f
lxlan <lxlandsx@outlook.com>
2022-04-15 10:58:13 +0800

    SpringBoot 配置的读取第三方Bean 的绑定和松散绑定的介绍

--- SpringBootBasicDemo/src/main/java/com/github/SpringBootBasicDemoApplication.java
+++ SpringBootBasicDemo/src/main/java/com/github/SpringBootBasicDemoApplication.java
@@ -3,6 +3,9 @@ package com.github;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author lxlan
  */
---
+++ SpringBoot_configuration/src/main/java/com/github/SpringBootConfigurationApplication.java
@@ -0,0 +1,33 @@
+package com.github;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.github.config.ServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 苏旭826
+ */
+@SpringBootApplication
+public class SpringBootConfigurationApplication {
+    @Bean
+    @ConfigurationProperties(prefix = "data-source")
+    public DruidDataSource dataSource() {
+        DruidDataSource ds = new DruidDataSource();
+        return ds;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(SpringBootConfigurationApplication.class, args);
+
+        ServerConfig bean = run.getBean(ServerConfig.class);
+        System.out.println(bean);
+
+        DruidDataSource bean1 = run.getBean(DruidDataSource.class);
+        System.out.println(bean1.getDriverClassName());
+    }
+
+}
