bd1675a33f46508adb9c62711669e4eb5812e888
lxlan <lxlandsx@outlook.com>
2022-04-15 20:32:19 +0800

    使用hibernate-validator来进行数据验证

--- SpringBoot_configuration/src/main/java/com/github/config/ServerConfig.java
+++ SpringBoot_configuration/src/main/java/com/github/config/ServerConfig.java
@@ -6,21 +6,28 @@ import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 /**
  * @author 苏旭826
- */
-
-/**
  * @Component 如果书写了 EnableConfigurationProperties，就不用在注册bean了
+ * 2. 开启对当前bean的属性注入校验
  */
 @Data
 @ConfigurationProperties(prefix = "servers")
+@Validated
 public class ServerConfig {
     private String ipAddress;
+    /**
+     * 3. 设置具体的规则
+     */
+    @Max(value = 8888, message = "最大值不能超过8888")
+    @Min(value = 2222, message = "最小值不能小于2222")
     private int port;
     private long timeout;
 
