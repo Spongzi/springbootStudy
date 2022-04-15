370799400fcf2e0545381e47fde98c3d8770c75b
lxlan <lxlandsx@outlook.com>
2022-04-15 20:18:45 +0800

    常用的计量单位设置

--- SpringBoot_configuration/src/main/java/com/github/SpringBootConfigurationApplication.java
+++ SpringBoot_configuration/src/main/java/com/github/SpringBootConfigurationApplication.java
@@ -5,6 +5,7 @@ import com.github.config.ServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Bean;
  * @author 苏旭826
  */
 @SpringBootApplication
+@EnableConfigurationProperties({ServerConfig.class})
 public class SpringBootConfigurationApplication {
     @Bean
     @ConfigurationProperties(prefix = "data-source")
--- SpringBoot_configuration/src/main/java/com/github/config/ServerConfig.java
+++ SpringBoot_configuration/src/main/java/com/github/config/ServerConfig.java
@@ -2,16 +2,31 @@ package com.github.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.boot.convert.DurationUnit;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * @author 苏旭826
  */
-@Component
+
+/**
+ * @Component 如果书写了 EnableConfigurationProperties，就不用在注册bean了
+ */
 @Data
 @ConfigurationProperties(prefix = "servers")
 public class ServerConfig {
     private String ipAddress;
     private int port;
     private long timeout;
+
+    @DurationUnit(ChronoUnit.MILLIS)
+    private Duration serverTimeout;
+
+    @DataSizeUnit(DataUnit.MEGABYTES)
+    private DataSize dataSize;
 }
