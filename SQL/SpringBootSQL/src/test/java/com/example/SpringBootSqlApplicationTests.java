b38e8997dac051fb1192164ac69caf5abd360b1f
lxlan <lxlandsx@outlook.com>
2022-04-17 21:03:54 +0800

    好

--- SQL/SpringBootSQL/src/test/java/com/example/SpringBootSqlApplicationTests.java
+++ SQL/SpringBootSQL/src/test/java/com/example/SpringBootSqlApplicationTests.java
@@ -1,9 +1,17 @@
 package com.example;
 
 import com.example.dao.BookDao;
+import com.example.daomain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class SpringBootSqlApplicationTests {
@@ -13,6 +21,37 @@ class SpringBootSqlApplicationTests {
 	@Test
 	void test () {
 		bookDao.selectById(1);
+	}
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	void testJdbcTemplate() {
+		String sqlStr = "select * from tb_book where id = 11;";
+		/*
+		List<Map<String, Object>> maps = jdbcTemplate.queryForList(sqlStr);
+		System.out.println(maps);
+		*/
+		RowMapper<Book> rm = new RowMapper<Book>() {
+			@Override
+			public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Book temp = new Book();
+				temp.setId(rs.getInt("id"));
+				temp.setName(rs.getString("name"));
+				temp.setType(rs.getString("type"));
+				temp.setDescription(rs.getString("description"));
+				return temp;
+			}
+		};
+		List<Book> query = jdbcTemplate.query(sqlStr, rm);
+		System.out.println(query);
+	}
+
+	@Test
+	void testJdbcTemplateSave() {
+		String sqlStr = "insert into tb_Book values (null, 'springboot', 'springboot', 'springboot');";
+		jdbcTemplate.update(sqlStr);
 	}
 
 }
