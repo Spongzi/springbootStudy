98a5920735f534b32496bc24ed40defeb89ed67e
lxlan <lxlandsx@outlook.com>
2022-04-17 14:47:55 +0800

    测试用例数据设定

--- SpringBootTest/src/test/java/com/example/PropertiesAndArgsTest.java
+++ SpringBootTest/src/test/java/com/example/PropertiesAndArgsTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import com.example.testcase.domain.BookCase;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,8 +21,12 @@ public class PropertiesAndArgsTest {
     @Value("${test.prop}")
     private String msg;
 
+    @Autowired
+    private BookCase bookCase;
+
     @Test
     void testProperties() {
         System.out.println(msg);
+        System.out.println(bookCase);
     }
 }
---
+++ SpringBootTest/src/test/java/com/example/testcase/domain/BookCase.java
@@ -0,0 +1,15 @@
+package com.example.testcase.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "testcase.book")
+public class BookCase {
+    private int id;
+    private String name;
+    private String uuid;
+    private long publishTime;
+}
