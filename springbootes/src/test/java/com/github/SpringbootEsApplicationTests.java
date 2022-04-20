7cba6cdae513be0415cdb801d6fc1c5e5f4f1806
unknown <lxlandsx@outlook.com>
2022-04-20 21:09:39 +0800

    SpringBoot整合ES 下

--- springbootes/src/test/java/com/github/SpringbootEsApplicationTests.java
+++ springbootes/src/test/java/com/github/SpringbootEsApplicationTests.java
@@ -1,19 +1,38 @@
 package com.github;
 
+import com.alibaba.fastjson.JSON;
+import com.github.dao.BookDao;
+import com.github.domain.Book;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.*;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootEsApplicationTests {
+
+    @Autowired
+    private BookDao bookDao;
 
     // 老版本！
     /*@Autowired
@@ -85,4 +104,70 @@ class SpringbootEsApplicationTests {
         request.source(json, XContentType.JSON);
         client.indices().create(request, RequestOptions.DEFAULT);
     }
+
+
+    /**
+     * 添加文档
+     */
+    @Test
+    void testCreateDoc() throws IOException {
+        Book book = bookDao.selectById(1);
+        IndexRequest request = new IndexRequest("books").id("1");
+        String json = JSON.toJSONString(book);
+        request.source(json, XContentType.JSON);
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+
+    /**
+     * 添加全文档
+     */
+    @Test
+    void testCreateDocAll() throws IOException {
+//        Book book = bookDao.selectById(1);
+        List<Book> books = bookDao.selectList(null);
+        BulkRequest bulkRequest = new BulkRequest();
+        for (Book book : books) {
+            IndexRequest request = new IndexRequest("books").id(book.getId().toString());
+            String json = JSON.toJSONString(book);
+            request.source(json, XContentType.JSON);
+            client.index(request, RequestOptions.DEFAULT);
+            // 在client添加了一个批处理
+            bulkRequest.add(request);
+        }
+
+        client.bulk(bulkRequest, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * 按id查询
+     */
+    @Test
+    void testGet() throws IOException {
+        GetRequest request = new GetRequest("books", "1");
+        GetResponse documentFields = client.get(request, RequestOptions.DEFAULT);
+        String sourceAsString = documentFields.getSourceAsString();
+        System.out.println(sourceAsString);
+    }
+
+    @Test
+    void testSearch() throws IOException {
+        SearchRequest request = new SearchRequest("books");
+
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.query(QueryBuilders.termQuery("name", "java"));
+        request.source(builder);
+
+        SearchResponse search = client.search(request, RequestOptions.DEFAULT);
+
+        // 得到查询到的所有数据的hits
+        SearchHits hits = search.getHits();
+
+        // 遍历得到hit
+        for (SearchHit hit : hits) {
+            String sourceAsString = hit.getSourceAsString();
+            System.out.println(sourceAsString);
+        }
+    }
+
 }
